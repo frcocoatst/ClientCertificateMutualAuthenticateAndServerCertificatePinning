9f523136b240063355e21423e98362557a1fc27f
Friedrich HAEUPL <friedrichhaeupl@friedrichs-imac-2.local>
2019-08-10 09:46:03 +0200

    Initial Commit

---
+++ ClientCertificateMutualAuth/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ClientCertificateMutualAuth
+//
+//  Created by Friedrich HAEUPL on 10.08.19.
+//  Copyright © 2019 Friedrich HAEUPL. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
